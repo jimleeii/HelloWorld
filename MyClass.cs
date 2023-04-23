d343f887c5e88503fa02acbc19cee160dab4a6c7
Wei Li <jimleeii@gmail.com>
2023-04-23 17:47:54 -0600

    first commit

---
+++ MyClass.cs
@@ -0,0 +1,7 @@
+namespace HelloWorld
+{
+    public class MyClass
+    {
+        public string Message => "Greating!";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            Console.WriteLine($"Hello World! {myClass.Message}");
+        }
+    }
+}
